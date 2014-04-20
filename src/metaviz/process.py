e98b265a2891781792004e50ca4e6eccf599113f
root <root@petal>
2014-04-20 10:03:52 -0400

    Now fully functional

--- src/metaviz/process.py
+++ src/metaviz/process.py
@@ -5,53 +5,74 @@ import exiftool
 import MySQLdb as mdb
 import hashlib
 
-# Establish Database Connection
 # Change 'yourpassword' to your mysql password
 con = mdb.connect('localhost', 'root', 'yourpassword', 'MetaViz');
 
-# Files being processed
-files = str(sys.argv)
+sys.stderr = open('/dev/null', 'w')
 
-# List of hashes that gets returned to PHP
-hahses = [];
+files = sys.argv
+files = files[1:]
 
-# SQL string to add value to database
-sql = "INSERT INTO images(%s) VALUES(%s)"
+hashes = [];
 
-
-def hashimage(image, hasher, blocksize=65536):
-	buf = image.read(blocksize)
-	while len(buf) > 0:
-		hasher.update(buf)
-		buf = image.read(blocksize)
-	return hasher.digest()
-
-
-# Checks the hash of each image being processed
-for image in files:
-	imagehash = hashimage(open(image, 'rb'), hashlib.sha256())
-	# If hash exists in database, remove from list of files
-	
+def key_exists(key):
+	with con:
+		cur = con.cursor()
+	sql = "SHOW COLUMNS FROM images LIKE " + "\'" + key + "\'"
+	cur.execute(sql)
+	result = cur.fetchall()
+	if len(result) > 0:
+		if key == result[0][0]:
+			return True
 	else:
-		args = 'name', image[14:] 
-		cur.execute(sql, args)
-		args = 'hash', imagehash
-		cur.execute(sql, args)
+		return False
 
+def get_hashes():
+	with con:
+		cur = con.cursor()
+	sql = "SELECT hash FROM images"
+	cur.execute(sql)
+	result = cur.fetchall()
+	return result
+
+def hashimage(image):
+	with open(image) as hash_me:
+		data = hash_me.read()
+		img_hash = hashlib.sha256(data).hexdigest()
+		return img_hash
+
+for image in files:
+	with con:
+		cur = con.cursor()
+	imagehash = hashimage(image)
+	sql_statement = "SELECT hash FROM images WHERE \'" + str(imagehash) +"\' IN (SELECT hash FROM images)"
+	check = cur.execute(sql_statement)
+	
+	if check == 0:
+		sql = "INSERT INTO images (hash) VALUES (\'" + imagehash + '\')'
+		cur.execute(sql)
+		sql2 = "INSERT INTO images (name) VALUES (\'" + image[15:] +'\')' 
+		#cur.execute(sql2)
+	else:
+		# Remove from list of files
+		pass
 	hashes.append(imagehash)
 
-# Pulls the metadata Out
 with exiftool.ExifTool() as et:
 	metadata = et.get_metadata_batch(files)
 
+i=0
 for d in metadata:
 	with con:
 		cur = con.cursor()
-	#Cycle through each dictonary and add each piece of metadata to the database
 	keys = list(d)
 	for key in keys:
-		args = key, keys[key]
-		cur.execute(sql, args) 
+		value = d[key]
+		key=key.split(':', 1)[-1]
+		key=key.replace('/','1')
+		if key_exists(key):
+			sql = "UPDATE images SET " + key + "=\'" + str(value) + "\' WHERE hash=\'" + hashes[i] + "\'"
+			cur.execute(sql) 
 
-# The hashes are the Unique ID to find the metadata from the databse
-return hashes;
+
+print hashes;
